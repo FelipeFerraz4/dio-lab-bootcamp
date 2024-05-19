a5acf327248dd7880c760096a52ca99ddc81cfdf
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2024-05-18 22:14:48 -0300

    create course class

---
+++ bootcamp/src/com/bluefox/model/Course.java
@@ -0,0 +1,29 @@
+package com.bluefox.model;
+
+public class Course extends Content {
+    
+    private int workload;
+    
+
+    public Course(String contentTitle, String contentDescription, int workload) {
+        super(contentTitle, contentDescription);
+        this.workload = workload;
+    }
+
+    @Override
+    public double calculeteXP() {
+        return this.getXpStandard() * workload;
+    }
+
+    public int getWorkload() {
+        return workload;
+    }
+
+    @Override
+    public String toString() {
+        return "Course [workload: " + workload + ", getContentId(): " + this.getContentId() + ", getContentTitle(): "
+                + this.getContentTitle() + ", getContentDescription(): " + this.getContentDescription() + "]";
+    }
+
+    
+}
